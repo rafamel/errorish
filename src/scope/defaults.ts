f9770a43ff00dad91ebd9d2c784083fc6794e9ce
Rafa Mel <melrcm@gmx.net>
2019-04-17 22:35:53 +0200

    feat(scope): adds defaults

---
+++ src/scope/defaults.ts
@@ -0,0 +1,17 @@
+import Errorish from '~/Errorish';
+import { ICoreOptions } from '~/types';
+
+/**
+ * Default values for error creation and normalization when an options object is not passed or lacks a particular property. See `ICoreOptions`.
+ */
+const defaults: Required<ICoreOptions> = {
+  name: 'Error',
+  message: 'An error ocurred',
+  Errorish: Errorish,
+  normalize: true,
+  capture: true,
+  allow: ['string'],
+  Error: Error
+};
+
+export default defaults;
