9cdd0ad423a9c67d8037d2f56421e6cb57471303
Rafa Mel <melrcm@gmx.net>
2019-04-17 22:48:25 +0200

    test(scope): adds tests for defaults

---
+++ test/scope/defaults.test.ts
@@ -0,0 +1,17 @@
+import defaults from '~/scope/defaults';
+
+test(`are defaults`, () => {
+  expect(defaults).toMatchInlineSnapshot(`
+    Object {
+      "Error": [Function],
+      "Errorish": [Function],
+      "allow": Array [
+        "string",
+      ],
+      "capture": true,
+      "message": "An error ocurred",
+      "name": "Error",
+      "normalize": true,
+    }
+  `);
+});
