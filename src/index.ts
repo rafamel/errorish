bec50c09ecabde68b6e64e3a111b8adb3f396387
Rafa Mel <melrcm@gmx.net>
2019-04-17 22:45:31 +0200

    feat: adds entry point exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-export default function main(): void {}
+export { default as ensure } from './ensure';
+export { default as throws } from './throws';
+export { default as rejects } from './rejects';
+export { default as normalize } from './normalize';
+export { default as Errorish } from './Errorish';
+export * from './types';
