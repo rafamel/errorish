f44a664dd4f268b97d8a923a31b73a4752a5ef72
Rafa Mel <melrcm@gmx.net>
2019-04-18 18:35:29 +0200

    feat(ensure): improves typings

--- src/ensure/index.ts
+++ src/ensure/index.ts
@@ -1,16 +1,22 @@
 import normalize from '~/normalize';
 import defaults from '~/scope/defaults';
 import trunk from './trunk';
+import Errorish from '~/Errorish';
 import { ICoreOptions, IOfType } from '~/types';
 
-export default function ensure(
-  error: any,
+/**
+ * Will return `error` if it is an instance of `options.Error` -by default, and instance of `Error`. Otherwise, it will instantiate and return an `Error` of class `options.Errorish` -`Errorish` by default. This newly created error will have:
+ *  - whatever you passed as an `error` as its `source` field.
+ *  - whatever you passed as `data` as its `data` field.
+ */
+export default function ensure<T>(
+  error: T,
   options?: ICoreOptions | null,
   data?: IOfType<any>
-): Error {
+): T extends Error ? T : Errorish<T> {
   const opts = Object.assign({}, defaults, options);
 
-  return opts.normalize
+  return (opts.normalize
     ? normalize(trunk(error, opts, data), opts)
-    : trunk(error, opts, data);
+    : trunk(error, opts, data)) as any;
 }
