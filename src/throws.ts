0e6d974438c5788ec8a631a94bc721cfda6c46c9
Rafa Mel <melrcm@gmx.net>
2019-04-27 03:51:10 +0200

    feat(throws): takes async functions too

--- src/throws.ts
+++ src/throws.ts
@@ -1,8 +1,9 @@
 import ensure from './ensure';
+import { isPromise, lazy } from 'promist';
 import { ICoreOptions, IOfType } from './types';
 
 /**
- * Returns the result of `fn`; if it throws, it will call `ensure` on the thrown error and throw it.
+ * Returns the result of `fn`; if it throws, it will call `ensure` on the thrown error and throw it. `fn` can be an *async* function -it will be automatically detected.
  */
 export default function throws<T>(
   fn: () => T,
@@ -10,7 +11,16 @@ export default function throws<T>(
   data?: IOfType<any>
 ): T {
   try {
-    return fn();
+    const res = fn();
+    if (!isPromise(res)) return res;
+
+    // if it is a promise:
+    // in case res was a lazy promise
+    return lazy((resolve, reject) => {
+      return (res as any)
+        .then(resolve)
+        .catch((err: Error) => reject(ensure(err, options, data)));
+    }) as any;
   } catch (err) {
     throw ensure(err, options, data);
   }
