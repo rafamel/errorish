f745eb957b3b69240075f16545c27d796161c1cb
Rafa Mel <melrcm@gmx.net>
2019-12-23 03:45:58 +0100

    chore: cleans up sources; begins complete rewrite
    BREAKING CHANGE: Errorish has been almost entirely rewritten. Please check the updated documentation
    -most of its functionality has suffered changes.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-export { default as scope } from './scope';
-export { default as ensure } from './ensure';
-export { default as throws } from './throws';
-export { default as rejects } from './rejects';
-export { default as normalize } from './normalize';
-export { default as Errorish } from './Errorish';
-export * from './types';
+export default function main(): null {
+  return null;
+}
--- src/scope/values.ts
+++
@@ -1,9 +0,0 @@
-import ensure from '~/ensure';
-import throws from '~/throws';
-import rejects from '~/rejects';
-import normalize from '~/normalize';
-import { Scope, CoreOptions } from '~/types';
-
-export const root: Scope = { name: null, ensure, throws, rejects, normalize };
-export const scopes: Record<string, Scope> = {};
-export const values: Record<string, CoreOptions> = {};
